3abc561a2b7e93600aac4227e7d0e2d41da58041
Felix Schlatter <web@felixschlatter.com>
2012-01-20 10:57:49 +0100

    framework fertig

--- js/script.js
+++ js/script.js
@@ -2,46 +2,79 @@
 Author: FelixSchlatter.com 
 
 */
+
+
+
+
+
 $(document).ready(function(){
 	
 var inn = 1.00;
 var out = 0.00;
 var speeder = 600;
 
+var images = ['bg_1.jpg', 'bg_2.jpg', 'bg_3.jpg', 'bg_4.jpg', 'bg_5.jpg'];
+$('header').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+
+$(window).resize(function() {
+	//$('header').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+});
+
+
+
+
+
+//$('.content').width($(window).width() - $('.left').width());
+
+$('#produkte').width($(window).width() - $('.left').width() - 200);
+$('#unternehmen').width($(window).width() - $('.left').width() - 200);
+$('#standort').width($(window).width() - $('.left').width() - 200);
+$('#kontakt').width($(window).width() - $('.left').width() - 200);
+
 $('#unternehmen, #standort, #kontakt').fadeTo(0, out);
 
 $('#nav1').click(function() {
-	$('#produkte').fadeTo('slow', inn);$('#unternehmen').fadeTo('slow', out);$('#standort').fadeTo('slow', out);$('#kontakt').fadeTo('slow', out);
+	$('#produkte').fadeTo('slow', inn);$('#unternehmen').fadeTo('slow', out);$('#standort').fadeTo('slow', out);$('#kontakt').fadeTo('slow', out);$('#produkte').height('100%');
+	//$('#kurve').animate({backgroundPosition: '-400px 138px'});
+	$('header').animate({backgroundPosition: '0px 0px'});
   $('#insideholder').animate({
-	left: '200'
+	left: $(window).width() -  $('#produkte').width() - 200
   }, speeder, function() {
     // finito!
   });
 });
 $('#nav2').click(function() {
-$('#produkte').fadeTo('slow', out);$('#unternehmen').fadeTo('slow', inn);$('#standort').fadeTo('slow', out);$('#kontakt').fadeTo('slow', out);
+$('#produkte').fadeTo('slow', out);$('#unternehmen').fadeTo('slow', inn);$('#standort').fadeTo('slow', out);$('#kontakt').fadeTo('slow', out);$('#produkte').height(50);
+//$('#kurve').animate({backgroundPosition: '-800px 138px'});
+$('header').animate({backgroundPosition: '-200px 0px'});
   $('#insideholder').animate({
-    left: '-545'
+    left: $(window).width() - $('#unternehmen').width() - $('#unternehmen').width() - 200
   }, speeder, function() {
     // finito!
   });
 });
 $('#nav3').click(function() {
-$('#produkte').fadeTo('slow', out);$('#unternehmen').fadeTo('slow', out);$('#standort').fadeTo('slow', inn);$('#kontakt').fadeTo('slow', out);
+$('#produkte').fadeTo('slow', out);$('#unternehmen').fadeTo('slow', out);$('#standort').fadeTo('slow', inn);$('#kontakt').fadeTo('slow', out);$('#produkte').height(50);
+//$('#kurve').animate({backgroundPosition: '-1200px 138px'});
+$('header').animate({backgroundPosition: '-400px 0px'});
   $('#insideholder').animate({
-    left: '-1290'
+    left: $(window).width() - $('#standort').width() - $('#standort').width() - $('#standort').width() - 200
   }, speeder, function() {
     // finito!
   });
 });
 $('#nav4').click(function() {
-$('#produkte').fadeTo('slow', out);$('#unternehmen').fadeTo('slow', out);$('#standort').fadeTo('slow', out);$('#kontakt').fadeTo('slow', inn);
+$('#produkte').fadeTo('slow', out);$('#unternehmen').fadeTo('slow', out);$('#standort').fadeTo('slow', out);$('#kontakt').fadeTo('slow', inn);$('#produkte').height(50);
+////$('#kurve').animate({backgroundPosition: '-1600px 138px'});
+$('header').animate({backgroundPosition: '-600px 0px'});
   $('#insideholder').animate({
-    left: '-2035'
+    left: $(window).width() - $('#kontakt').width()  - $('#kontakt').width()  - $('#kontakt').width()  - $('#kontakt').width()  -200
   }, speeder, function() {
     // finito!
   });
 });
+
+
 
 })
 
